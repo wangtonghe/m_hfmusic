b49ba8e1e71d3268f2fd915aba4e8f040259dfb0
wangtonghe <wangtonghe@nggirl.com.cn>
2016-04-26 17:39:23 +0800

    无改变

---
+++ src/main/java/top/wthfeng/service/SingerService.java
@@ -0,0 +1,14 @@
+package top.wthfeng.service;
+
+/**
+ * @author Liii
+ * @date 2016/3/1 18:13
+ */
+public interface SingerService {
+    /**
+     *
+     */
+     void list();
+
+    void select();
+}
---
+++ src/main/java/top/wthfeng/service/impl/SingerServiceImpl.java
@@ -0,0 +1,19 @@
+package top.wthfeng.service.impl;
+
+import top.wthfeng.service.SingerService;
+
+/**
+ * @author Liii
+ * @date 2016/3/1 18:14
+ */
+public class SingerServiceImpl implements SingerService {
+    @Override
+    public void select() {
+
+    }
+
+    @Override
+    public void list() {
+
+    }
+}
